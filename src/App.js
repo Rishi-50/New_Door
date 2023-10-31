df1fcc9eff6c68e95b4d13e48722ce160f00b0e6
Rishi Savla <114904283+Rishi-50@users.noreply.github.com>
2023-10-31 22:27:46 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import Header from './Components/Header';
+import Hero from './Components/Hero';
+import Companies from './Components/Companies';
+import Residencies from './Components/Residencies';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Hero />
+      <Companies />
+      <Residencies />
+    </div>
+  );
+}
+
+export default App;
